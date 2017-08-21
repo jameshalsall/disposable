9a520283665cac0efcd002d154d80b6cd13ed76e
James Halsall <james.t.halsall@googlemail.com>
2017-08-21 21:17:14 +0100

    Add basic calculator stub

---
+++ lib/calculator/outgoings_calculator.rb
@@ -0,0 +1,17 @@
+#
+# Author:: James Halsall <james.t.halsall@googlemail.com>
+# Copyright:: 2017 rights reserved.
+# License:: MIT
+#
+
+#
+# Outgoings calculator
+#
+# Responsible foe calculating the remaining outgoings from
+# an array of outgoing data items.
+#
+class OutgoingsCalculator
+  def self.remainingOutgoings(dayNumber, outgoings)
+    return 0
+  end
+end
--- lib/disposable.rb
+++ lib/disposable.rb
@@ -1,3 +1,24 @@
+#
+# Author:: James Halsall <james.t.halsall@googlemail.com>
+# Copyright:: 2017 rights reserved.
+# License:: MIT
+#
+
+require 'Date'
+require './lib/calculator/outgoings_calculator.rb'
+
+#
+# Disposable.
+#
+# Disposable tells you your remaining disposable income by taking
+# your scheduled monthly outgoings from a CSV file and deducating it
+# from your current account balance
+#
 class Disposable
-  #todo
+  def calculateRemaining(outgoings_data_file, current_balance)
+    # TODO: parse the CSV data
+    outgoings = OutgoingsCalculator.remainingOutgoings(Date.today.strftime("%d"), [])
+
+    return current_balance.to_i - outgoings
+  end
 end
